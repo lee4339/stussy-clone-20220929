44e1c34f78be65fbabba301e9a949be8568b07a2
lee4339 <skyho4339@naver.com>
2022-09-30 12:57:33 +0900

    스프링부트220930(스투시클론-로그인 및 회원가입 디자인, 시큐리티, 회원가입 요청날리기)

---
+++ src/main/java/com/stussy/stussyclone20220929/StussyClone20220929Application.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20220929;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StussyClone20220929Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StussyClone20220929Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/stussy/stussyclone20220929/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.stussy.stussyclone20220929.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 기존의 WebSecurityConfigurerAdapter 클래스를 해당 SecurityConfig로 대체하겠다.
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.httpBasic().disable(); // httpBasic() 인증방식 비활성화
+        http.authorizeRequests() // 모든 요청시에 실행을 해라
+                .antMatchers("/test/**", "/index/**") // 해당 요청 주소들은
+                .authenticated() // 인증(로그인)을 통해 위 페이지에 들어가게 함(인증이 필요하다)
+                .anyRequest() // antMatchers 외에 다른 모든 요청에
+                .permitAll() // 모두 접근 권한을 허용해라
+                .and() // 그리고  // and 대신 http.로 다시 시작가능
+                .formLogin() // 폼로그인 방식으로 인증을 해라
+                .loginPage("/account/login") // 우리가 만든 로그인 페이지를 사용해라.
+                .defaultSuccessUrl("/index"); // 로그인 성공 후
+
+
+
+
+    }
+}
