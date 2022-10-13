392de979402a5c6744cdefaadd56945420ec12ef
lee4339 <skyho4339@naver.com>
2022-10-13 12:56:37 +0900

    스프링부트221012(스투시클론-admin 상품조회 페이지)

--- src/main/java/com/stussy/stussyclone20220929/repository/admin/ProductRepository.java
+++ src/main/java/com/stussy/stussyclone20220929/repository/admin/ProductRepository.java
@@ -5,10 +5,12 @@ import com.stussy.stussyclone20220929.domain.ProductImgFile;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface ProductRepository {
 
     public int saveProduct(Product product) throws  Exception;
     public int saveImgFiles(List<ProductImgFile> product_img_files) throws Exception;
+    public List<Product> getProductList(Map<String, Object> map) throws Exception;
 }
